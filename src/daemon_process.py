a4f3385f0ff1f2fdf117f6022e415129ab834f89
liwanjun <liwanjun@lvwan.com>
2024-09-14 10:25:23 +0800

    add ai news

--- src/daemon_process.py
+++ src/daemon_process.py
@@ -13,7 +13,7 @@ from report_generator import ReportGenerator  # 导入报告生成器类
 from llm import LLM  # 导入语言模型类，可能用于生成报告内容
 from subscription_manager import SubscriptionManager  # 导入订阅管理器类，管理GitHub仓库订阅
 from logger import LOG  # 导入日志记录器
-
+from AI_news_client import AINewsClient  # 导入 AI 新闻客户端
 
 def graceful_shutdown(signum, frame):
     # 优雅关闭程序的函数，处理信号时调用
@@ -32,13 +32,11 @@ def github_job(subscription_manager, github_client, report_generator, notifier, 
         notifier.notify_github_report(repo, report)
     LOG.info(f"[定时任务执行完毕]")
 
-
 def hn_topic_job(hacker_news_client, report_generator):
     LOG.info("[开始执行定时任务]Hacker News 热点话题跟踪")
     markdown_file_path = hacker_news_client.export_top_stories()
     _, _ = report_generator.generate_hn_topic_report(markdown_file_path)
     LOG.info(f"[定时任务执行完毕]")
-
 
 def hn_daily_job(hacker_news_client, report_generator, notifier):
     LOG.info("[开始执行定时任务]Hacker News 今日前沿技术趋势")
@@ -51,6 +49,15 @@ def hn_daily_job(hacker_news_client, report_generator, notifier):
     notifier.notify_hn_report(date, report)
     LOG.info(f"[定时任务执行完毕]")
 
+def ai_news_job(ai_news_client, report_generator, notifier):
+    LOG.info("[开始执行定时任务]AI 新闻总结")
+    news_file_path = ai_news_client.get_latest_news_file()
+    if news_file_path:
+        report, _ = report_generator.generate_ai_news_report(news_file_path)
+        notifier.notify_ai_news_report(report)
+    else:
+        LOG.warning("没有找到最新的 AI 新闻文件")
+    LOG.info("[定时任务执行完毕]AI 新闻总结")
 
 def main():
     # 设置信号处理器
@@ -62,11 +69,16 @@ def main():
     notifier = Notifier(config.email)  # 创建通知器实例
     llm = LLM(config)  # 创建语言模型实例
     report_generator = ReportGenerator(llm, config.report_types)  # 创建报告生成器实例
+    LOG.info(f"report_generator: {config.report_types}")
+
     subscription_manager = SubscriptionManager(config.subscriptions_file)  # 创建订阅管理器实例
+    ai_news_client = AINewsClient()  # 创建 AI 新闻客户端实例
 
     # 启动时立即执行（如不需要可注释）
     # github_job(subscription_manager, github_client, report_generator, notifier, config.freq_days)
-    hn_daily_job(hacker_news_client, report_generator, notifier)
+    #hn_topic_job(hacker_news_client, report_generator)
+    #hn_daily_job(hacker_news_client, report_generator, notifier)
+    ai_news_job(ai_news_client, report_generator, notifier)
 
     # 安排 GitHub 的定时任务
     schedule.every(config.freq_days).days.at(
@@ -78,6 +90,9 @@ def main():
 
     # 安排 hn_daily_job 每天早上10点执行一次
     schedule.every().day.at("10:00").do(hn_daily_job, hacker_news_client, report_generator, notifier)
+
+    # 安排 AI 新闻任务每天执行一次，比如在每天下午 2 点
+    schedule.every().day.at("14:00").do(ai_news_job, ai_news_client, report_generator, notifier)
 
     try:
         # 在守护进程中持续运行
